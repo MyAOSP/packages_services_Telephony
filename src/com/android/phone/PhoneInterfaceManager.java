7c09ad7b43a77dde8068d36a5b5adcf1a5015f9f
baked <travp624@gmail.com>
2014-06-13 03:29:10 -0500

    Phone: Enable LTE for GSM LTE phones (part I)
    Change-Id: I580c37f3272c8cc8363e8db5b1c227bf6f74a04a

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -322,6 +322,10 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
         mApp.startActivity(intent);
     }
 
+    public void toggleLTE(boolean on) {
+        return;
+    }
+
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
                                            boolean showDialpad) {
         if (!PhoneGlobals.sVoiceCapable) {
@@ -930,6 +934,10 @@ public class PhoneInterfaceManager extends ITelephony.Stub implements CallModele
      */
     public int getLteOnCdmaMode() {
         return mPhone.getLteOnCdmaMode();
+    }
+
+    public int getLteOnGsmMode() {
+        return mPhone.getLteOnGsmMode();
     }
 
     @Override
